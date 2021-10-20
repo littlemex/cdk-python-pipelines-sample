cd9e1a8422640ce0f7f12ed0ef76bd2609d2bacb
akzw <you@example.com>
2021-10-20 01:13:21 +0000

    MESSAGE

--- sample/sample/pipeline_stack.py
+++ sample/sample/pipeline_stack.py
@@ -1,8 +1,10 @@
 from aws_cdk import (
     core,
     aws_codecommit as codecommit,
+    aws_codepipeline as codepipeline,
+    aws_codepipeline_actions as codepipeline_actions,
+    pipelines as pipelines
 )
-# from pipeline_stage import PipelineStage
 
 class PipelineStack(core.Stack):
 
@@ -12,5 +14,37 @@ class PipelineStack(core.Stack):
         # Creates a CodeCommit repository called 'WorkshopRepo'
         repo = codecommit.Repository(
             self, 'SampleRepo',
-            repository_name= "SampleRepo"
+            repository_name = "SampleRepo"
+        )
+        # Defines the artifact representing the sourcecode
+        source_artifact = codepipeline.Artifact()
+        # Defines the artifact representing the cloud assembly
+        # (cloudformation template + all other assets)
+        cloud_assembly_artifact = codepipeline.Artifact()
+
+        pipeline = pipelines.CdkPipeline(
+            self, 'Pipeline',
+            cloud_assembly_artifact=cloud_assembly_artifact,
+
+
+            source_action = codepipeline_actions.GitHubSourceAction(
+                action_name='Github',
+                owner='lettlemex',
+                repo='cdk-python-pipelines-sample',
+                branch='feature/init',
+                oauth_token=self.node.try_get_context("oauth_token"), # FIXME: 後でsecretから取るようにする
+                trigger=codepipeline_actions.GitHubTrigger.POLL,
+                output=source_artifact,
+            ),
+
+            # Builds our source code outlined above into a could assembly artifact
+            synth_action=pipelines.SimpleSynthAction(
+                install_commands=[
+                    'npm install -g aws-cdk', # Installs the cdk cli on Codebuild
+                    'pip install -r requirements.txt' # Instructs codebuild to install required packages
+                ],
+                synth_command='npx cdk synth',
+                source_artifact=source_artifact, # Where to get source code to build
+                cloud_assembly_artifact=cloud_assembly_artifact, # Where to place built source
+            )
         )
