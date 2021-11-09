2ceb6e93233b351368dc76e40e9163f9ace42dd4
akzw <you@example.com>
2021-11-09 02:28:46 +0000

    github連携

--- sample/sample/pipeline_stack.py
+++ sample/sample/pipeline_stack.py
@@ -3,39 +3,47 @@ from aws_cdk import (
     aws_codecommit as codecommit,
     aws_codepipeline as codepipeline,
     aws_codepipeline_actions as codepipeline_actions,
-    pipelines as pipelines
+    pipelines as pipelines,
+    aws_secretsmanager as secretmanager
 )
+
+import aws_cdk.core as cdk
 
 class PipelineStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # Creates a CodeCommit repository called 'WorkshopRepo'
-        repo = codecommit.Repository(
-            self, 'SampleRepo',
-            repository_name = "SampleRepo"
+        secret = secretmanager.Secret.from_secret_attributes(self, 'Secret',
+            secret_arn='arn:aws:secretsmanager:us-east-1:067150986393:secret:githubtoken-yPq40Q' # FIXME
         )
-        # Defines the artifact representing the sourcecode
-        source_artifact = codepipeline.Artifact()
-        # Defines the artifact representing the cloud assembly
-        # (cloudformation template + all other assets)
-        cloud_assembly_artifact = codepipeline.Artifact()
+
+        source_artifact = codepipeline.Artifact('artifact1')
+
+        # ソースアクション作成
+        repository_name = 'cdk-python-pipelines-sample'
+        owner = 'littlemex';
+        oauth_token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+        branch = 'feature/init';
+
+        source_action = codepipeline_actions.GitHubSourceAction(
+            action_name='Github',
+            owner='littlemex',
+            repo='cdk-python-pipelines-sample',
+            branch='feature/init',
+            oauth_token=secret.secret_value.to_string(),
+            trigger=codepipeline_actions.GitHubTrigger.POLL,
+            output=source_artifact,
+        ),
+
+        # hoge
+
+        cdk.CfnOutput(self, 'hogehoge', value='hoge')
 
         pipeline = pipelines.CdkPipeline(
             self, 'Pipeline',
-            cloud_assembly_artifact=cloud_assembly_artifact,
-
-
-            source_action = codepipeline_actions.GitHubSourceAction(
-                action_name='Github',
-                owner='lettlemex',
-                repo='cdk-python-pipelines-sample',
-                branch='feature/init',
-                oauth_token=self.node.try_get_context("oauth_token"), # FIXME: 後でsecretから取るようにする
-                trigger=codepipeline_actions.GitHubTrigger.POLL,
-                output=cloud_assembly_artifact,
-            ),
+            cloud_assembly_artifact=source_artifact,
+            source_action=source_action,
 
             # Builds our source code outlined above into a could assembly artifact
             synth_action=pipelines.SimpleSynthAction(
@@ -45,6 +53,6 @@ class PipelineStack(core.Stack):
                 ],
                 synth_command='npx cdk synth',
                 source_artifact=source_artifact, # Where to get source code to build
-                cloud_assembly_artifact=cloud_assembly_artifact, # Where to place built source
+                cloud_assembly_artifact=source_artifact, # Where to place built source
             )
         )
